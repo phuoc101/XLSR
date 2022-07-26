a775d6a4402c09c359da3310fdc8e6e5646a567f
phuoc101 <nguyenthuanphuoc101@protonmail.com>
2022-07-26 16:17:49 +0300

    added webcam test

---
+++ webcam.py
@@ -0,0 +1,143 @@
+import os
+import argparse
+import torch
+import time
+import cv2
+import torchvision.transforms as transforms
+
+from model import XLSR
+from dataset import create_dataloader
+from metric import cal_psnr
+from visualization import save_res
+
+
+def test(model, device):
+    with torch.no_grad():
+        print("warm up ...")
+        random_input = torch.randn(1, 3, 640, 360).to(device)
+        # warm up
+        for _ in range(10):
+            model(random_input)
+
+        with torch.autograd.profiler.profile() as prof:
+            model(random_input)
+        print(prof.key_averages().table(sort_by="self_cpu_time_total"))
+        transform = transforms.ToTensor()
+        # print("Start testing the model speed on 640*360 input ...")
+        # test_t = 0.
+        # for idx in range(100):
+        #     if device != 'cpu':
+        #         torch.cuda.synchronize()
+        #     t0 = time.perf_counter()
+        #     model(random_input)
+        #     if device != 'cpu':
+        #         torch.cuda.synchronize()
+        #     t1 = time.perf_counter()
+        #     print(f"Inference #{idx}, inference time: {1000*(t1-t0):.2f}ms")
+        #     test_t += t1 - t0
+        # print(f"Average inference time on 640*360 input: {1000*test_t/100:.2f}ms")
+        # with open(txt_path, 'a') as f:
+        #     f.write(f"Average inference time on 640*360 input: {1000*test_t/100:.2f}ms" + '\n')
+        #
+        print("Start the inference ...")
+        cam = cv2.VideoCapture(0)
+        while True:
+            ret, frame = cam.read()
+            # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            # frame = cv2.resize(frame, (320, 240))
+            if ret:
+                LR_img = transform(frame)
+                LR_img = LR_img[None]
+                LR_img = LR_img.to(device).float()
+                if device != 'cpu':
+                    torch.cuda.synchronize()
+                t0 = time.perf_counter()
+                HR_pred = model(LR_img)
+                pred_img = HR_pred[0].cpu().numpy().transpose(1,2,0)
+                if device != 'cpu':
+                    torch.cuda.synchronize()
+                t1 = time.perf_counter()
+                inference_time = t1 - t0
+                scaled = cv2.resize(pred_img, (640, 480))
+                cv2.imshow("orig", frame)
+                cv2.imshow("scale", scaled)
+                cv2.imshow("sisr", pred_img)
+                if cv2.waitKey(1) == ord('q'):
+                    break
+            else:
+                pass
+
+        # for LR_img, HR_img, img_name in dataloader:
+        #     LR_img, HR_img = LR_img.to(device).float(), HR_img.to(device).float()
+        #     if device != 'cpu':
+        #         torch.cuda.synchronize()
+        #     t0 = time.perf_counter()
+        #     HR_pred = model(LR_img)
+        #     if device != 'cpu':
+        #         torch.cuda.synchronize()
+        #     t1 = time.perf_counter()
+        #     psnr = cal_psnr(HR_pred, HR_img).item()
+        #     inference_time = t1 - t0
+        #     print(f"PSRN on {img_name}: {psnr:.3f}, inference time: {1000*inference_time:.2f}ms")
+        #     with open(txt_path, 'a') as f:
+        #         f.write(f"PSRN on {img_name}: {psnr:.3f}, inference time: {1000*inference_time:.2f}ms" + '\n')
+        #     avg_psnr += psnr
+        #     avg_time += inference_time
+        #     pred_list.append(HR_pred)
+        #     name_list += img_name
+    # avg_psnr /= len(test_dataloader)
+    # avg_time /= len(test_dataloader)
+    # print(f"Average PSRN: {avg_psnr:.3f}, average inference time: {1000*avg_time:.2f}ms")
+    # with open(txt_path, 'a') as f:
+    #     f.write(f"Average PSRN: {avg_psnr:.3f}, average inference time: {1000*avg_time:.2f}ms")
+    # return pred_list, name_list
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--save-dir', type=str,
+                        default='exp/OneCyclicLR', help='hyperparameters path')
+    parser.add_argument('--SR-rate', type=int, default=3,
+                        help='the scale rate for SR')
+    parser.add_argument('--model', type=str, default='',
+                        help='the path to the saved model')
+    parser.add_argument('--device', type=str,
+                        default='cpu', help='gpu id or "cpu"')
+    opt = parser.parse_args()
+
+    os.makedirs(opt.save_dir, exist_ok=True)
+
+    # cuDnn configurations
+    if opt.device != 'cpu':
+        torch.backends.cudnn.benchmark = True
+        torch.backends.cudnn.deterministic = True
+
+    # txt file to record training process
+    # txt_path = os.path.join(opt.save_dir, 'test_res.txt')
+    # if os.path.exists(txt_path):
+    #     os.remove(txt_path)
+
+    # folder to save the predicted HR image in the validation
+    test_folder = os.path.join(opt.save_dir, 'test_res')
+    os.makedirs(test_folder, exist_ok=True)
+
+    device = 'cuda:' + str(opt.device) if opt.device != 'cpu' else 'cpu'
+    model = XLSR(opt.SR_rate)
+
+    # load pretrained model
+    if opt.model.endswith('.pt') and os.path.exists(opt.model):
+        model.load_state_dict(torch.load(opt.model, map_location=device))
+    else:
+        model.load_state_dict(torch.load(os.path.join(
+            opt.save_dir, 'best.pt'), map_location=device))
+    model.to(device)
+    model.eval()
+
+    # test_dataloader = create_dataloader('test', opt.SR_rate, False, batch_size=1, shuffle=False, num_workers=1)
+
+    # evaluate
+    test(model, device)
+
+    # print("Saving the predicted HR images")
+    # save_res(pred_list, name_list, test_folder)
+    # print(f"Testing is done!, predicted HR images are saved in {test_folder}")
